179ade258462d95e269e94cff9c5b90478126572
root <root@ip-172-31-20-111.ec2.internal>
2025-01-28 05:41:49 +0000

    hello chaitanya

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class newCode{
        public static void main(String[] args){
        
-              System.out.print("Hello world");
+              System.out.print("Hello Chaitanya");
        
        }
 
