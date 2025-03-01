e2f6f8a5118615ff7ae87809a2dacde4c3e477ca
Chaitanya1465 <saichaitanya1465@gmail.com>
2025-03-01 12:08:10 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class newCode{
        public static void main(String[] args){
        
-              System.out.print("Hello Chaitanya ok");
+              System.out.print("Hello Chaitanya");
        
        }
 
